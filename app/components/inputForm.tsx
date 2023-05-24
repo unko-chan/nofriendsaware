3f7e9e10ca84cdb23259afde6c9169735a9dbe86
Kevin Liang <kevinliang0303@gmail.com>
2023-05-24 14:40:25 -0700

    initial commit

---
+++ app/components/inputForm.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  message: z.string().min(2, {
+    message: "Message must be at least 2 characters.",
+  }),
+  id: z.string().default(() => Math.random().toString(36).substring(2)),
+  timestamp: z.string().default(() => new Date().toISOString()),
+  name: z.string().optional(),
+  city: z.string().optional(),
+});
+
+export function InputForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+      name: "",
+      city: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='message'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Input placeholder='Enter your message' {...field} />
+              </FormControl>
+              <FormDescription>This is your message.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder='Enter your name' {...field} />
+              </FormControl>
+              <FormDescription>This is your name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='city'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>City</FormLabel>
+              <FormControl>
+                <Input placeholder='Enter your city' {...field} />
+              </FormControl>
+              <FormDescription>This is your city.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+}
